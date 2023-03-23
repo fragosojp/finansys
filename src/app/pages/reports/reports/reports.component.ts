07897672370b1aa43a1f3e56e462518e82c761f9
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-03-23 13:41:58 -0300

    Feat: Add reports tamplete

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.css']
+  styleUrls: ['./reports.component.css'],
 })
 export class ReportsComponent {
-
+  generateReports(): void {}
 }
