73346de2b910299a5568173ac06f91436b2b00eb
Joao Fragoso <joaopaulosoaresfragoso@gmail.com>
2023-03-22 22:46:45 -0300

    Feat: extraing navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,16 +5,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { InMemoryDatabase } from '../in-memory-database';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
   ],
-  exports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  exports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NavbarComponent,
+  ],
 })
 export class CoreModule {}
